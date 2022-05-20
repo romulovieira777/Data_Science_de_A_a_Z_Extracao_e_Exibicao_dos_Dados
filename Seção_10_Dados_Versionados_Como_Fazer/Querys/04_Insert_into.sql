8443684b9d380357c4026d80736c2c5eda549cd4
Romulo Vieira <romulo.vieira777@gmail.com>
2022-05-20 18:10:08 -0300

    Dados Versionados - Como Fazer?

---
+++ Seção_10_Dados_Versionados_Como_Fazer/Querys/04_Insert_into.sql
@@ -0,0 +1,17 @@
+INSERT INTO [DS_TRAINNING].[dbo].[WRK_PRODUTOS_ESTOQUE]
+(
+	[CODIGO]
+  , [DESC_PROUTO]
+  , [DESC_ABV]
+  , [Unidades]
+  , [INI_VIGENCIA]
+)
+VALUES
+(
+	?
+  , ?
+  , ?
+  , ?
+  , ?
+)
+GO
